9e0c64eb170b9c0f32bf8e741a824328811f8e09
Karl Cayme <kcayme@agsx.net>
2025-01-09 17:40:16 +0800

    feat: updated catppuccin config

--- nvim/lua/kickstart/plugins/themes/catpuccin.lua
+++ nvim/lua/kickstart/plugins/themes/catpuccin.lua
@@ -2,7 +2,6 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-
   config = function()
     require("catppuccin").setup({
       flavour = "mocha", -- latte, frappe, macchiato, mocha
@@ -16,14 +15,14 @@ return {
       dim_inactive = {
         enabled = true, -- dims the background color of inactive window
         shade = "dark",
-        percentage = 0.05, -- percentage of the shade to apply to the inactive window
+        percentage = 0.6, -- percentage of the shade to apply to the inactive window
       },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
+      -- no_italic = false, -- Force no italic
+      -- no_bold = false, -- Force no bold
+      -- no_underline = false, -- Force no underline
       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
+        conditionals = {},
         loops = {},
         functions = {},
         keywords = {},
@@ -99,7 +98,7 @@ return {
         },
         cmp = true,
         gitsigns = true,
-        nvimtree = true,
+        nvimtree = false,
         treesitter = true,
         notify = true,
         mini = {
