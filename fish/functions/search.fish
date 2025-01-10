b60b1d04730ede353081fb799034d4673ff8573e
Karl Cayme <kcayme@agsx.net>
2025-01-10 13:50:21 +0800

    feat: new search alias

--- fish/functions/search.fish
+++ fish/functions/search.fish
@@ -1,4 +1,4 @@
-function search --wraps=fzf\ --preview\ \'bat\ --color=always\ \{\}\' --description alias\ search=fzf\ --preview\ \'bat\ --color=always\ \{\}\'
-  fzf --preview 'bat --color=always {}' $argv
+function search --wraps='nvim ' --wraps='nvim config.fish' --wraps='nvim fzf' --wraps=nvim\ \(fzf\ --preview=\'bat\ --color=always\ \{\}\'\) --description alias\ search=nvim\ \(fzf\ --preview=\'bat\ --color=always\ \{\}\'\)
+  nvim (fzf --preview='bat --color=always {}') $argv
         
 end
