92877eba0d1e1c58aff0e86adcf78bb68c0e5944
Karl <kcayme@gmail.com>
2025-01-15 20:38:51 +0800

    nvim: updated configs

--- nvim/lua/kickstart/plugins/core/telescope.lua
+++ nvim/lua/kickstart/plugins/core/telescope.lua
@@ -80,13 +80,13 @@ return {
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind  existing buffers" })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set("n", "<leader>/", function()
+      vim.keymap.set("n", "<leader>f/", function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
           winblend = 2,
           previewer = true,
         }))
-      end, { desc = "[/] Fuzzily search in current buffer" })
+      end, { desc = "[F]uzzily search [/] in current buffer" })
 
       -- Also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
