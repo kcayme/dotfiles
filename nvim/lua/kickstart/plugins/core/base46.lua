9a0bbf98cf4b4566ba6fb6490c87faec84f6c35b
Karl <kcayme@gmail.com>
2025-01-17 00:54:10 +0800

    NVChad base46 plugin

---
+++ nvim/lua/kickstart/plugins/core/base46.lua
@@ -0,0 +1,18 @@
+return {
+  -- { "nvim-tree/nvim-web-devicons", lazy = true },
+  -- {
+  --   "nvchad/ui",
+  --   config = function()
+  --     require("nvchad")
+  --   end,
+  -- },
+  {
+    "nvchad/base46",
+    lazy = false,
+    priority = 1000,
+    build = function()
+      require("base46").load_all_highlights()
+      vim.cmd.colorscheme("nvchad")
+    end,
+  },
+}
