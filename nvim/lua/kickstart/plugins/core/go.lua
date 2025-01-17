b7bc649ba90326701ea4ea1b05bd80b4cd31128a
Karl Cayme <kcayme@agsx.net>
2025-01-17 10:12:02 +0800

    update nvim configs

--- nvim/lua/kickstart/plugins/core/go.lua
+++ nvim/lua/kickstart/plugins/core/go.lua
@@ -5,9 +5,22 @@ return {
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
+      "folke/trouble.nvim",
     },
     config = function()
-      require("go").setup()
+      require("go").setup({
+        trouble = true,
+        -- verbose = false,
+        -- diagnostic = false,
+        -- diagnostic = { -- set diagnostic to false to disable vim.diagnostic.config setup,
+        -- true: default nvim setup
+        -- hdlr = true, -- hook lsp diag handler and send diag to quickfix
+        -- underline = true,
+        -- virtual_text = { spacing = 2, prefix = "" }, -- virtual text setup
+        -- signs = { "", "", "", "" }, -- set to true to use default signs, an array of 4 to specify custom signs
+        -- update_in_insert = false,
+        -- },
+      })
       local map = vim.keymap.set
       map("n", "<leader>got", "<cmd>GoTestFunc<cr>", {})
     end,
