d0108af21d17d0b7f06fa035630b9d50a99ed6bb
Karl <kcayme@gmail.com>
2025-01-18 22:43:17 +0800

    nvim: vscode profile disabled gopls autocmd

--- nvim/lua/code/config/autocmds.lua
+++ nvim/lua/code/config/autocmds.lua
@@ -6,11 +6,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
-local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    require("go.format").goimports()
-  end,
-  group = format_sync_grp,
-})
+-- local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     require("go.format").goimports()
+--   end,
+--   group = format_sync_grp,
+-- })
