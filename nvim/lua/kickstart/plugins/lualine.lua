ffd606b74d77dedf07b98883f83f5f1de4962220
Karl Cayme <kcayme@agsx.net>
2025-02-02 20:09:32 +0800

    nvim: replaced lualine for nvchad statusline

--- nvim/lua/kickstart/plugins/lualine.lua
+++ nvim/lua/kickstart/plugins/lualine.lua
@@ -1,81 +1,80 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local trouble = require("trouble")
-      local symbols = trouble.statusline({
-        mode = "lsp_document_symbols",
-        groups = {},
-        title = false,
-        filter = { range = true },
-        format = "{kind_icon}{symbol.name:Normal}",
-        -- The following line is needed to fix the background color
-        -- Set it to the lualine section you want to use
-        hl_group = "lualine_c_normal",
-      })
-
-      require("lualine").setup({
-        options = {
-          icons_enabled = true,
-          theme = "catppuccin",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-          },
-        },
-
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = {
-            "FugitiveHead",
-            "branch",
-            "diff",
-            "diagnostics",
-          },
-          lualine_c = {
-            "filename",
-            {
-              symbols.get,
-              cond = symbols.has,
-            },
-          },
-          lualine_x = {
-            "searchcount",
-            "encoding",
-          },
-          lualine_y = {
-            "fileformat",
-            "filetype",
-            "progress",
-          },
-          lualine_z = {
-            "location",
-          },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
-      })
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   config = function()
+  --     local trouble = require("trouble")
+  --     local symbols = trouble.statusline({
+  --       mode = "lsp_document_symbols",
+  --       groups = {},
+  --       title = false,
+  --       filter = { range = true },
+  --       format = "{kind_icon}{symbol.name:Normal}",
+  --       -- The following line is needed to fix the background color
+  --       -- Set it to the lualine section you want to use
+  --       hl_group = "lualine_c_normal",
+  --     })
+  --
+  --     require("lualine").setup({
+  --       options = {
+  --         icons_enabled = true,
+  --         theme = "auto",
+  --         component_separators = { left = "", right = "" },
+  --         section_separators = { left = "", right = "" },
+  --         disabled_filetypes = {
+  --           statusline = {},
+  --           winbar = {},
+  --         },
+  --         ignore_focus = {},
+  --         always_divide_middle = true,
+  --         globalstatus = false,
+  --         refresh = {
+  --           statusline = 1000,
+  --           tabline = 1000,
+  --           winbar = 1000,
+  --         },
+  --       },
+  --
+  --       sections = {
+  --         lualine_a = { "mode" },
+  --         lualine_b = {
+  --           "branch",
+  --           "diff",
+  --           "diagnostics",
+  --         },
+  --         lualine_c = {
+  --           "filename",
+  --           {
+  --             symbols.get,
+  --             cond = symbols.has,
+  --           },
+  --         },
+  --         lualine_x = {
+  --           "searchcount",
+  --           "encoding",
+  --         },
+  --         lualine_y = {
+  --           "fileformat",
+  --           "filetype",
+  --           "progress",
+  --         },
+  --         lualine_z = {
+  --           "location",
+  --         },
+  --       },
+  --       inactive_sections = {
+  --         lualine_a = {},
+  --         lualine_b = {},
+  --         lualine_c = { "filename" },
+  --         lualine_x = { "location" },
+  --         lualine_y = {},
+  --         lualine_z = {},
+  --       },
+  --       tabline = {},
+  --       winbar = {},
+  --       inactive_winbar = {},
+  --       extensions = {},
+  --     })
+  --   end,
+  -- },
 }
