1ac2811c2128c8088296a94115cddc9649bd30aa
Karl Cayme <kcayme@agsx.net>
2025-02-04 10:28:17 +0800

    refactor: moved blink.cmp to autocompletion.lua

---
+++ nvim/lua/kickstart/plugins/autocompletion.lua
@@ -0,0 +1,251 @@
+return {
+  {
+    "saghen/blink.cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+      },
+      {
+        "saghen/blink.compat",
+        opts = {},
+        version = "*",
+        lazy = false,
+      },
+      -- "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+    },
+
+    version = "*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- fallback
+      keymap = {
+        preset = "super-tab",
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<Esc>"] = { "hide", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+        -- ["<C-n>"] = { "snippet_forward", "fallback" },
+        -- ["<C-p>"] = { "snippet_backward", "fallback" },
+        -- ["<Up>"] = { "select_prev", "fallback" },
+        -- ["<Down>"] = { "select_next", "fallback" },
+      },
+
+      completion = {
+        list = { selection = { preselect = true, auto_insert = true } },
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        ghost_text = { enabled = false },
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          auto_show = true,
+          draw = {
+            treesitter = { "lsp" },
+            columns = {
+              { "label", "label_description", gap = 2 },
+              { "kind_icon", "kind", gap = 1 },
+            },
+          },
+        },
+      },
+
+      signature = { enabled = true },
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+
+      snippets = {
+        preset = "luasnip",
+        expand = function(args)
+          return require("luasnip").lsp_expand(args)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require("luasnip").jumpable(filter.direction)
+          end
+          return require("luasnip").in_snippet()
+        end,
+        jump = function(direction)
+          require("luasnip").jump(direction)
+        end,
+      },
+
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "nvim_lsp" },
+        providers = {
+          lsp = {
+            name = "lsp",
+            enabled = true,
+            module = "blink.cmp.sources.lsp",
+            score_offset = 100,
+          },
+          snippets = {
+            name = "snippets",
+            enabled = true,
+            module = "blink.cmp.sources.snippets",
+            score_offset = 900, -- the higher the number, the higher the priority
+          },
+          path = {
+            name = "Path",
+            module = "blink.cmp.sources.path",
+            score_offset = 500,
+            enabled = true,
+            fallbacks = { "snippets", "buffer" },
+            opts = {
+              trailing_slash = false,
+              label_trailing_slash = true,
+              get_cwd = function(context)
+                return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+              end,
+              show_hidden_files_by_default = true,
+            },
+          },
+          buffer = {
+            name = "Buffer",
+            enabled = true,
+            max_items = 3,
+            module = "blink.cmp.sources.buffer",
+            score_offset = 600, -- the higher the number, the higher the priority
+          },
+          nvim_lsp = {
+            name = "cmp_nvim_lsp",
+            enabled = true,
+            module = "blink.compat.source",
+            score_offset = 950,
+          },
+          -- cmp_path = {
+          --   name = "path",
+          --   enabled = true,
+          --   module = "blink.compat.source",
+          -- },
+        },
+        cmdline = {},
+      },
+    },
+    opts_extend = { "sources.default", "sources.compat" },
+  },
+
+  -- { -- Autocompletion
+  --   "hrsh7th/nvim-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = {
+  --     -- Snippet Engine & its associated nvim-cmp source
+  --     {
+  --       "L3MON4D3/LuaSnip",
+  --       build = (function()
+  --         -- Build Step is needed for regex support in snippets
+  --         -- This step is not supported in many windows environments
+  --         -- Remove the below condition to re-enable on windows
+  --         if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+  --           return
+  --         end
+  --         return "make install_jsregexp"
+  --       end)(),
+  --     },
+  --     "saadparwaiz1/cmp_luasnip",
+  --     "hrsh7th/cmp-nvim-lsp",
+  --     "hrsh7th/cmp-path",
+  --     "rafamadriz/friendly-snippets",
+  --   },
+  --
+  --   config = function()
+  --     -- See `:help cmp`
+  --     local cmp = require("cmp")
+  --     local luasnip = require("luasnip")
+  --     luasnip.config.setup({})
+  --     require("luasnip.loaders.from_vscode").lazy_load()
+  --     local has_words_before = function()
+  --       unpack = unpack or table.unpack
+  --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  --     end
+  --
+  --     cmp.setup({
+  --       snippet = {
+  --         expand = function(args)
+  --           luasnip.lsp_expand(args.body)
+  --         end,
+  --       },
+  --       window = {
+  --         completion = cmp.config.window.bordered(),
+  --         documentation = cmp.config.window.bordered(),
+  --       },
+  --       completion = { completeopt = "menu,menuone,noinsert" },
+  --       mapping = cmp.mapping.preset.insert({
+  --         ["<C-j>"] = cmp.mapping.select_next_item(),
+  --         ["<C-k>"] = cmp.mapping.select_prev_item(),
+  --         ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  --         ["<C-space>"] = cmp.mapping.complete({}),
+  --         -- Think of <c-l> as moving to the right of your snippet expansion.
+  --         --  So if you have a snippet that's like:
+  --         --  function $name($args)
+  --         --    $body
+  --         --  end
+  --         --
+  --         -- <c-l> will move you to the right of each of the expansion locations.
+  --         -- <c-h> is similar, except moving you backwards.
+  --         ["<C-l>"] = cmp.mapping(function()
+  --           if luasnip.expand_or_locally_jumpable() then
+  --             luasnip.expand_or_jump()
+  --           end
+  --         end, { "i", "s" }),
+  --         ["<C-h>"] = cmp.mapping(function()
+  --           if luasnip.locally_jumpable(-1) then
+  --             luasnip.jump(-1)
+  --           end
+  --         end, { "i", "s" }),
+  --
+  --         -- Super Tab
+  --         ["<Tab>"] = cmp.mapping(function(fallback)
+  --           if cmp.visible() then
+  --             cmp.select_next_item()
+  --           -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+  --           -- that way you will only jump inside the snippet region
+  --           elseif luasnip.expand_or_jumpable() then
+  --             luasnip.expand_or_jump()
+  --           elseif has_words_before() then
+  --             cmp.complete()
+  --           else
+  --             fallback()
+  --           end
+  --         end, { "i", "s" }),
+  --
+  --         ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --           if cmp.visible() then
+  --             cmp.select_prev_item()
+  --           elseif luasnip.jumpable(-1) then
+  --             luasnip.jump(-1)
+  --           else
+  --             fallback()
+  --           end
+  --         end, { "i", "s" }),
+  --       }),
+  --       sources = {
+  --         { name = "nvim_lsp" },
+  --         { name = "luasnip" },
+  --         { name = "path" },
+  --         { name = "buffer" },
+  --       },
+  --     })
+  --   end,
+  -- },
+}
