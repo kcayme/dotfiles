a1839844213d0ef2140992ddad68b435a64331a9
Karl Cayme <kcayme@agsx.net>
2025-02-05 10:50:42 +0800

    updated Comment keymaps

--- nvim/lua/kickstart/plugins/comment.lua
+++ nvim/lua/kickstart/plugins/comment.lua
@@ -7,8 +7,8 @@ return {
       require("Comment").setup()
 
       local map = vim.api.nvim_set_keymap
-      map("n", "<C-/>", "gcc", { noremap = false, desc = "Toggle linewise comment" })
-      map("v", "<C-/>", "gb", { noremap = false, desc = "Toggle blockwise comment" })
+      map("n", "<C-_>", "gcc", { noremap = false, desc = "Toggle linewise comment" })
+      map("v", "<C-_>", "gb", { noremap = false, desc = "Toggle blockwise comment" })
     end,
   },
 }
