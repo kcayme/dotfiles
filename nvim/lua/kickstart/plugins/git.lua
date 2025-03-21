ea179cdffc55e870af146bba2dbd620bde6a331c
Karl <kcayme@gmail.com>
2025-03-21 22:06:11 +0800

    nvim: chore proper importing

--- nvim/lua/kickstart/plugins/git.lua
+++ nvim/lua/kickstart/plugins/git.lua
@@ -1,21 +1,23 @@
 -- git-related plugins
 return {
-  "kdheepak/lazygit.nvim",
-  cmd = {
-    "LazyGit",
-    -- "LazyGitConfig",
-    -- "LazyGitCurrentFile",
-    -- "LazyGitFilter",
-    -- "LazyGitFilterCurrentFile",
-  },
-  -- optional for floating window border decoration
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      -- "LazyGitConfig",
+      -- "LazyGitCurrentFile",
+      -- "LazyGitFilter",
+      -- "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
   },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
