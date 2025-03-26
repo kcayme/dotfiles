e378c07a5f7bb4e70c1e4ee0a9c829af6c36ce88
Karl <kcayme@gmail.com>
2025-03-27 01:02:59 +0800

    nvim: autocmd to update nvim-tree cursor when moving between files

--- nvim/lua/kickstart/config/autocmds.lua
+++ nvim/lua/kickstart/config/autocmds.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  desc = "Change the cursor in the tree for the current focused buffer",
+  group = vim.api.nvim_create_augroup("nvim-tree-cursor-focus", { clear = true }),
+  callback = function()
+    local api = require("nvim-tree.api")
+    api.tree.find_file({ open = false, focus = false })
+  end,
+})
