682d480c7f7b69c1e08f899a55f2157d961be79e
Karl <kcayme@gmail.com>
2025-03-30 01:03:09 +0800

    nvim: update command for diagnostic navigation

--- nvim/lua/kickstart/config/keymaps.lua
+++ nvim/lua/kickstart/config/keymaps.lua
@@ -15,8 +15,16 @@ map("n", "<Esc>", function()
 end, { desc = "Clear notifs and highlights" })
 
 -- ============ DIAGNOSTICS ============
-map("n", "<leader>[", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-map("n", "<leader>]", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+-- map("n", "<leader>[", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+-- map("n", "<leader>]", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+map("n", "<leader>[", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Go to previous [D]iagnostic message" })
+
+map("n", "<leader>]", function()
+  vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Go to next [D]iagnostic message" })
+
 map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
