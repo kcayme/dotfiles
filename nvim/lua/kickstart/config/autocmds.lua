d8a229830f71b452afbdb234e805fa69bdfa9b3b
Karl <kcayme@gmail.com>
2025-03-30 23:46:26 +0800

    nvim: add nil check

--- nvim/lua/kickstart/config/autocmds.lua
+++ nvim/lua/kickstart/config/autocmds.lua
@@ -19,7 +19,7 @@ vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client:supports_method("textDocument/foldingRange") then
+    if client and client:supports_method("textDocument/foldingRange") then
       local win = vim.api.nvim_get_current_win()
       vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
     end
