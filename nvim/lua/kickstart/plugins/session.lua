6b3c7fd5f52be0ff5861822235e8a0d2e11806a1
Karl <kcayme@gmail.com>
2025-03-31 12:50:53 +0800

    nvim: temporarily replace auto-session repo

--- nvim/lua/kickstart/plugins/session.lua
+++ nvim/lua/kickstart/plugins/session.lua
@@ -1,6 +1,9 @@
 return {
   {
-    "rmagatti/auto-session",
+    -- "rmagatti/auto-session",
+    "cameronr/auto-session",
+    -- branch = "[restore-error-handler](https://github.com/cameronr/auto-session/compare/restore-error-handler)",
+    branch = "restore-error-handler",
     config = function()
       require("auto-session").setup({
         log_level = "error",
