91659e3c1952135cdc899d86e39cd03cfd57d750
Karl <kcayme@gmail.com>
2025-03-31 13:11:21 +0800

    nvim: update bufferline config

--- nvim/lua/kickstart/plugins/bufferline.lua
+++ nvim/lua/kickstart/plugins/bufferline.lua
@@ -5,13 +5,25 @@ return {
       require("bufferline").setup({
         options = {
           themable = true,
-          numbers = "buffer_id",
+          -- numbers = "both",
+          numbers = function(opts)
+            return string.format("%s  %s", opts.ordinal, opts.id)
+          end,
           always_show_bufferline = true,
           separator_style = "slant",
           diagnostics = "nvim_lsp",
           diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            local icon = level:match("error") and " " or " "
-            return " " .. icon .. count
+            -- local icon = level:match("error") and " " or " "
+            -- return " " .. icon .. count
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and " " or (e == "warning" and " " or " ")
+              s = s .. n .. sym
+              if e == "error" then
+                s = s .. count
+              end
+            end
+            return s
           end,
           --separator_style = "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
           indicator = {
