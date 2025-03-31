f2f508e008e921d56320ddebd5b01bd304ff7671
Karl <kcayme@gmail.com>
2025-03-31 22:25:18 +0800

    nvim: replace tailwind-sorter with tailwind-tools

--- nvim/lua/kickstart/plugins/tailwind-sorter.lua
+++ nvim/lua/kickstart/plugins/tailwind-sorter.lua
@@ -1,13 +1,12 @@
 return {
-  "laytan/tailwind-sorter.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
-  build = "cd formatter && npm ci && npm run build",
-  config = function()
-    -- require("tailwind-sorter").setup({
-    --   on_save_enabled = true, -- If `true`, automatically enables on save sorting.
-    --   on_save_pattern = { "*.html", "*.js", "*.jsx", "*.tsx", "*.twig", "*.hbs", "*.php", "*.heex", "*.astro" }, -- The file patterns to watch and sort.
-    --   node_path = "node",
-    --   trim_spaces = false, -- If `true`, trim any extra spaces after sorting.
-    -- })
-  end,
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {}, -- your configuration
+  },
 }
