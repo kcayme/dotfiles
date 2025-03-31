8c014e2a8363982d82533e66ad5a97e92122f3ca
Karl <kcayme@gmail.com>
2025-03-31 22:12:18 +0800

    nvim: disabled nvim-spectre

--- nvim/lua/kickstart/config/keymaps.lua
+++ nvim/lua/kickstart/config/keymaps.lua
@@ -107,16 +107,16 @@ map({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc
 map({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
 map({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
 
--- nvim-spectre
-map("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
-  desc = "Toggle Spectre",
-})
-map("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-  desc = "Search current word",
-})
-map("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-  desc = "Search current word",
-})
-map("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-  desc = "Search on current file",
-})
+-- -- nvim-spectre
+-- map("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+--   desc = "Toggle Spectre",
+-- })
+-- map("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+--   desc = "Search current word",
+-- })
+-- map("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+--   desc = "Search current word",
+-- })
+-- map("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+--   desc = "Search on current file",
+-- })
