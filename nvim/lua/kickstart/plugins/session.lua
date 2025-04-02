5c9e4a2f3b644ee5a40b99602c6f923ed53cac6c
Karl <kcayme@gmail.com>
2025-04-03 06:49:01 +0800

    nvim: updated dependencies

--- nvim/lua/kickstart/plugins/session.lua
+++ nvim/lua/kickstart/plugins/session.lua
@@ -1,9 +1,6 @@
 return {
   {
-    -- "rmagatti/auto-session",
-    "cameronr/auto-session",
-    -- branch = "[restore-error-handler](https://github.com/cameronr/auto-session/compare/restore-error-handler)",
-    branch = "restore-error-handler",
+    "rmagatti/auto-session",
     config = function()
       require("auto-session").setup({
         log_level = "error",
