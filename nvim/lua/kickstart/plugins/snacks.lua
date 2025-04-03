d86e75651cf828e5fdb74283e8b3ad11c15394f7
Karl Cayme <kcayme@agsx.net>
2025-04-03 17:44:14 +0800

    nvim: enable snacks.scroll

--- nvim/lua/kickstart/plugins/snacks.lua
+++ nvim/lua/kickstart/plugins/snacks.lua
@@ -24,6 +24,12 @@ return {
         },
       },
       statuscolumn = { enabled = true },
+      scroll = {
+        animate = {
+          duration = { step = 100, total = 50 },
+          easing = "linear",
+        },
+      },
       picker = {
         files = {},
         smart = {},
