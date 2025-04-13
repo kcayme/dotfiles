9e693621c8b3161980b3b1c6d72ec0038f625836
Karl <kcayme@gmail.com>
2025-04-14 01:01:53 +0800

    nvim: updated blink.cmp config

--- nvim/lua/kickstart/plugins/autocompletion.lua
+++ nvim/lua/kickstart/plugins/autocompletion.lua
@@ -21,9 +21,7 @@ return {
         version = "*",
         lazy = false,
       },
-      -- "saadparwaiz1/cmp_luasnip",
-      -- "hrsh7th/cmp-nvim-lsp",
-      -- "hrsh7th/cmp-path",
+      { "onsails/lspkind.nvim" },
     },
 
     version = "*",
@@ -63,8 +61,42 @@ return {
           draw = {
             treesitter = { "lsp" },
             columns = {
-              { "label", "label_description", gap = 2 },
-              { "kind_icon", "kind", gap = 1 },
+              { "kind_icon", gap = 1 },
+              { "label", "label_description", "kind", gap = 1 },
+            },
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local lspkind = require("lspkind")
+                  local icon = ctx.kind_icon
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      icon = dev_icon
+                    end
+                  else
+                    icon = require("lspkind").symbolic(ctx.kind, {
+                      mode = "symbol",
+                    })
+                  end
+
+                  return icon .. ctx.icon_gap
+                end,
+
+                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- You can also add the same function for `kind.highlight` if you want to
+                -- keep the highlight groups in sync with the icons.
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      hl = dev_hl
+                    end
+                  end
+                  return hl
+                end,
+              },
             },
           },
         },
@@ -150,8 +182,8 @@ return {
         },
         -- cmdline = {},
       },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    opts_extend = { "sources.default", "sources.compat" },
-    fuzzy = { implementation = "prefer_rust_with_warning" },
+    opts_extend = { "sources.default" },
   },
 }
