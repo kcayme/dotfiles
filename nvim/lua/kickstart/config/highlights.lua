715123bfba85e7824a2cac18a20a03b823ea8d99
Karl <kcayme@gmail.com>
2025-04-20 17:31:14 +0800

    nvim: refactor declaration of vim.g.base64_cache

--- nvim/lua/kickstart/config/highlights.lua
+++ nvim/lua/kickstart/config/highlights.lua
@@ -1,4 +1,5 @@
 -- apply nvchad base46
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
 for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
   dofile(vim.g.base46_cache .. v)
 end
