e4fdb8fe3bc56f4b46af0486c27d9d2edaae98e9
Karl <kcayme@gmail.com>
2025-04-20 17:28:42 +0800

    nvim: refactor lazy setup for vscode and kickstart

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,1 @@
--- Load appropriate plugins, settings, and keymaps
-if vim.g.vscode then
-  require("code.init")
-  print("vscode config loaded")
-else
-  require("kickstart.init")
-  print("default config loaded")
-end
+require("setup")
---
+++ nvim/lua/setup.lua
@@ -0,0 +1,79 @@
+-- setup options before lazy loading
+if vim.g.vscode then
+  require("code.config.options")
+else
+  require("kickstart.config.options")
+end
+
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- { import = "kickstart.themes.tokyonight" },
+    {
+      import = "kickstart.themes.base46",
+      cond = function()
+        return not vim.g.vscode
+      end,
+    },
+    {
+      import = "kickstart.plugins",
+      cond = function()
+        return not vim.g.vscode
+      end,
+    },
+    {
+      import = "code.plugins46",
+      cond = function()
+        return vim.g.vscode
+      end,
+    },
+  },
+  checker = { enabled = true },
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
+})
+
+-- Load appropriate plugins, settings, and keymaps
+if vim.g.vscode then
+  require("code.config.keymaps")
+  require("code.config.autocmds")
+  print("vscode config loaded")
+else
+  require("kickstart.config.options")
+  require("kickstart.config.autocmds")
+  require("kickstart.config.highlights")
+  require("kickstart.config.keymaps")
+  print("default config loaded")
+end
