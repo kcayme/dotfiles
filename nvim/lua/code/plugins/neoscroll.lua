a02c0dc022dfb664782d599bab9091c402db58c9
Karl <kcayme@gmail.com>
2025-04-20 17:28:42 +0800

    nvim: refactor lazy setup for vscode and kickstart

--- nvim/lua/code/plugins/neoscroll.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "karb94/neoscroll.nvim",
-    config = function()
-        require('neoscroll').setup({
-            mappings = { '<C-u>', '<C-d>' },
-            respect_scrolloff = true
-        })
-    end
-}
