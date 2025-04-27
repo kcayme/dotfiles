f24d48bc2c0e244a6421835895b1c4a507a1989f
Karl <kcayme@gmail.com>
2025-04-27 22:03:43 +0800

    feat: set prettierd as json formatter

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -24,6 +24,7 @@ return {
         go = { "goimports", "gofmt", stop_after_first = true },
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         css = { "prettierd", stop_after_first = true },
