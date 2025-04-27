7cb91d7e9f217cc1197bd2a584ac2ba21a0f39f0
Karl <kcayme@gmail.com>
2025-04-27 22:03:26 +0800

    refactor: move lint autocmds to autocmds.lua

--- nvim/lua/core/plugins/lsp.lua
+++ nvim/lua/core/plugins/lsp.lua
@@ -133,33 +133,8 @@ return {
         lua = { "luacheck" },
         go = { "golangcilint" },
         python = { "pylint" },
+        json = { "jsonlint" },
       }
-
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-      vim.api.nvim_create_autocmd({
-        -- "BufEnter",
-        "BufWritePost",
-        "InsertLeave",
-      }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-          vim.diagnostic.config({ virtual_text = false })
-        end,
-      })
-      --
-      -- Show linters for the current buffer's file type
-      vim.api.nvim_create_user_command("LintInfo", function()
-        local filetype = vim.bo.filetype
-        local linters = require("lint").linters_by_ft[filetype]
-
-        if linters then
-          print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
-        else
-          print("No linters configured for filetype: " .. filetype)
-        end
-      end, {})
 
       -- Toggle linting
       vim.keymap.set("n", "<leader>lt", function()
