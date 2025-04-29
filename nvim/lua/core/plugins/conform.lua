14bca14696c53313545a78fa3d9eb03a4148e17f
Karl <kcayme@gmail.com>
2025-04-29 11:53:52 +0800

    remove: json formatter

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -26,7 +26,7 @@ return {
         typescript = { "prettierd", "prettier", stop_after_first = true },
         html = { "prettierd", "prettier", stop_after_first = true },
         css = { "prettierd", stop_after_first = true },
-        json = { "prettierd", "prettier", stop_after_first = true },
+        -- json = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         yaml = { "prettierd", stop_after_first = true },
