93ad8395848051a503052bb06137ea1b4e0251dd
Karl <kcayme@gmail.com>
2025-05-01 13:10:22 +0800

    refactor: custom highlights to base46 hl_add

--- nvim/lua/core/config/highlights.lua
+++ nvim/lua/core/config/highlights.lua
@@ -5,31 +5,31 @@ for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
 end
 
 -- support undercurl on diagnostic highlights
-local colors = require("base46").get_theme_tb("base_30")
+-- local colors = require("base46").get_theme_tb("base_30")
+--
+-- local hl_color_map = {
+--   {
+--     group_name = "DiagnosticUnderlineError",
+--     opts = { sp = colors.red },
+--   },
+--   {
+--     group_name = "DiagnosticUnderlineWarn",
+--     opts = { sp = colors.eared },
+--   },
+--   {
+--     group_name = "DiagnosticUnderlineInfo",
+--     opts = { sp = colors.red },
+--   },
+--   {
+--     group_name = "DiagnosticUnderlineHint",
+--     opts = { sp = colors.red },
+--   },
+--   {
+--     group_name = "BlinkCmpGhostText",
+--     opts = { fg = "#4e5665" },
+--   },
+-- }
 
-local hl_color_map = {
-  {
-    group_name = "DiagnosticUnderlineError",
-    opts = { sp = colors.red },
-  },
-  {
-    group_name = "DiagnosticUnderlineWarn",
-    opts = { sp = colors.red },
-  },
-  {
-    group_name = "DiagnosticUnderlineInfo",
-    opts = { sp = colors.red },
-  },
-  {
-    group_name = "DiagnosticUnderlineHint",
-    opts = { sp = colors.red },
-  },
-  {
-    group_name = "BlinkCmpGhostText",
-    opts = { fg = "#4e5665" },
-  },
-}
-
-for _, value in ipairs(hl_color_map) do
-  vim.api.nvim_set_hl(0, value.group_name, value.opts)
-end
+-- for _, value in ipairs(hl_color_map) do
+--   vim.api.nvim_set_hl(0, value.group_name, value.opts)
+-- end
