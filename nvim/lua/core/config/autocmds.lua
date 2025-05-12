a56d6694d168e060237727442860ba78b4a9e647
Karl <kcayme@gmail.com>
2025-05-13 00:06:17 +0800

    feat: add snacks.rename autocmd

--- nvim/lua/core/config/autocmds.lua
+++ nvim/lua/core/config/autocmds.lua
@@ -219,3 +219,18 @@ vim.api.nvim_create_user_command("LintInfo", function()
     print("No linters configured for filetype: " .. filetype)
   end
 end, {})
+
+-- https://github.com/folke/snacks.nvim/blob/main/docs/rename.md
+local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
+vim.api.nvim_create_autocmd("User", {
+  pattern = "NvimTreeSetup",
+  callback = function()
+    local events = require("nvim-tree.api").events
+    events.subscribe(events.Event.NodeRenamed, function(data)
+      if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
+        data = data
+        Snacks.rename.on_rename_file(data.old_name, data.new_name)
+      end
+    end)
+  end,
+})
