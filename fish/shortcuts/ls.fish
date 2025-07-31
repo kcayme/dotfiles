3bb81be8ce84fd35e7bb181175c326f97a128e52
Karl <kcayme@gmail.com>
2025-07-31 19:21:53 +0800

    feat: add fish shortcuts sample

---
+++ fish/shortcuts/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza -h -l' --description 'alias ls=eza -h -l'
+  eza -h -l $argv
+        
+end
---
+++ fish/shortcuts/tks.fish
@@ -0,0 +1,4 @@
+function tks --wraps='tmux kill-server' --description 'alias tks=tmux kill-server'
+  tmux kill-server $argv
+        
+end
