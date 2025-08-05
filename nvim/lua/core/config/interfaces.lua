643ededcd5419f997a0ad9c4f87edbdc7fbb9dc6
Karl <kcayme@gmail.com>
2025-08-05 23:13:58 +0800

    feat: add interface for common pickers

--- nvim/lua/core/config/interfaces.lua
+++ nvim/lua/core/config/interfaces.lua
@@ -1,82 +1,30 @@
-local picker = {
-  backend = "fzf-lua",
-  library = require("fzf-lua"),
+local backends = {
+  fff = require("core.config.backends.fff"),
+  snacks = require("core.config.backends.snacks"),
+  fzf = require("core.config.backends.fzf"),
 }
 
-picker.lsp_implementations = function(opts)
-  local backend = picker.backend
+local Picker = {
+  default_backend = "fzf", -- fallback
+}
 
-  if backend == "fzf-lua" or backend == "snacks" then
-    picker.library.lsp_implementations(opts)
-  else
-    error("Unsupported picker backend: " .. tostring(picker.engine))
-  end
+local function get_backend(opts)
+  local name = opts and opts.backend or Picker.default_backend
+  local backend = backends[name]
+  assert(backend, "Unsupported backend: " .. tostring(name))
+  return backend
 end
 
-picker.lsp_type_definitions = function(opts)
-  local backend = picker.backend
+-- Use metatable to forward undefined methods
+setmetatable(Picker, {
+  __index = function(_, method_name)
+    return function(opts)
+      local backend = get_backend(opts)
+      local method = backend[method_name]
+      assert(method, "Method not found in backend: " .. method_name)
+      return method(opts)
+    end
+  end,
+})
 
-  if backend == "fzf-lua" then
-    picker.library.lsp_typedefs(opts)
-  elseif picker.backend == "snacks" then
-    picker.library.lsp_type_definitions(opts)
-  else
-    error("Unsupported picker backend: " .. tostring(picker.engine))
-  end
-end
-
-picker.lsp_symbols = function(opts)
-  local backend = picker.backend
-
-  if backend == "fzf-lua" then
-    require(backend).lsp_document_symbols(opts)
-  elseif picker.backend == "snacks" then
-    require(backend).lsp_symbols(opts)
-  else
-    error("Unsupported picker backend: " .. tostring(picker.engine))
-  end
-end
-
-picker.lsp_workspace_symbols = function(opts)
-  local backend = picker.backend
-
-  if backend == "fzf-lua" then
-    require(backend).lsp_workspace_symbols(opts)
-  elseif picker.backend == "snacks" then
-    require(backend).lsp_workspace_symbols(opts)
-  else
-    error("Unsupported picker backend: " .. tostring(picker.engine))
-  end
-end
-
-picker.lsp_declarations = function(opts)
-  local backend = picker.backend
-
-  if backend == "fzf-lua" or backend == "snacks" then
-    require(backend).lsp_declarations(opts)
-  else
-    error("Unsupported picker backend: " .. tostring(picker.engine))
-  end
-end
-
-picker.lsp_definitions = function(opts)
-  local backend = picker.backend
-
-  if backend == "fzf-lua" or backend == "snacks" then
-    require(backend).lsp_definitions(opts)
-  else
-    error("Unsupported picker backend: " .. tostring(picker.engine))
-  end
-end
-
-picker.lsp_references = function(opts)
-  local backend = picker.backend
-
-  if backend == "fzf-lua" or backend == "snacks" then
-    require(backend).lsp_references(opts)
-  else
-    error("Unsupported picker backend: " .. tostring(picker.engine))
-  end
-end
-
-return picker
+return Picker
