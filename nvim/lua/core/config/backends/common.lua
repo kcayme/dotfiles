9b6c1913fa5e5bfcf197a97203bf5a747edf11ae
Karl <kcayme@gmail.com>
2025-08-05 23:13:45 +0800

    feat: implement picker backends

---
+++ nvim/lua/core/config/backends/common.lua
@@ -0,0 +1,23 @@
+-- Common passthrough methods
+local picker_interface_methods = {
+  "lsp_implementations",
+  "lsp_type_definitions",
+  "lsp_symbols",
+  "lsp_workspace_symbols",
+  "lsp_declarations",
+  "lsp_definitions",
+  "lsp_references",
+  "grep",
+  "grep_word",
+  "todo_comments",
+  "diagnostics",
+  "help",
+  "jumps",
+  "keymaps",
+  "files",
+  "buffers",
+  "smart", -- frecency
+  "global", -- fzf-lua specific
+}
+
+return picker_interface_methods
