bf06e55eed8b5d388a88b7de6017cf3a3c43a8e3
Karl <kcayme@gmail.com>
2025-08-09 15:32:14 +0800

    feat: add fish prompt samples

---
+++ fish/shortcuts/fish_prompt.fish
@@ -0,0 +1,3 @@
+function fish_prompt
+    # Left prompt empty
+end
---
+++ fish/shortcuts/fish_right_prompt.fish
@@ -0,0 +1,19 @@
+function fish_right_prompt
+    # switch $fish_bind_mode
+    #     case default
+    #         set_color green
+    #         echo -n "NORMAL"
+    #     case insert
+    #         set_color blue
+    #         echo -n "INSERT"
+    #     case replace_one
+    #         set_color red
+    #         echo -n "REPLACE"
+    #     case visual
+    #         set_color magenta
+    #         echo -n "VISUAL"
+    # end
+    # echo "$fish_bind_mode"
+    # set_color normal
+    # fish_default_mode_prompt
+end
