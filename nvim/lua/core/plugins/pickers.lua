e5facdf3ab32330da8df5c406f5622017ab5a3d1
Karl <kcayme@gmail.com>
2025-08-10 16:07:48 +0800

    feat: override fzf-lua keymaps

--- nvim/lua/core/plugins/pickers.lua
+++ nvim/lua/core/plugins/pickers.lua
@@ -294,6 +294,46 @@ return {
         fzf = {
           match = "NvChHeadyellow",
         },
+        keymap = {
+          builtin = {
+            ["<M-Esc>"] = "hide", -- hide fzf-lua, `:FzfLua resume` to continue
+            ["<F1>"] = "toggle-help",
+            ["<F2>"] = "toggle-fullscreen",
+            -- Only valid with the 'builtin' previewer
+            ["<F3>"] = "toggle-preview-wrap",
+            ["<F4>"] = "toggle-preview",
+            -- Rotate preview clockwise/counter-clockwise
+            ["<F5>"] = "toggle-preview-ccw",
+            ["<F6>"] = "toggle-preview-cw",
+            -- `ts-ctx` binds require `nvim-treesitter-context`
+            ["<F7>"] = "toggle-preview-ts-ctx",
+            ["<F8>"] = "preview-ts-ctx-dec",
+            ["<F9>"] = "preview-ts-ctx-inc",
+            ["<S-Left>"] = "preview-reset",
+            ["<C-d>"] = "preview-page-down",
+            ["<C-u>"] = "preview-page-up",
+            ["<M-S-down>"] = "preview-down",
+            ["<M-S-up>"] = "preview-up",
+          },
+          fzf = {
+            -- fzf '--bind=' options
+            -- true,        -- uncomment to inherit all the below in your custom config
+            ["ctrl-z"] = "abort",
+            ["ctrl-u"] = "unix-line-discard",
+            ["ctrl-f"] = "half-page-down",
+            ["ctrl-b"] = "half-page-up",
+            ["ctrl-a"] = "beginning-of-line",
+            ["ctrl-e"] = "end-of-line",
+            ["alt-a"] = "toggle-all",
+            ["alt-g"] = "first",
+            ["alt-G"] = "last",
+            -- Only valid with fzf previewers (bat/cat/git/etc)
+            ["f3"] = "toggle-preview-wrap",
+            ["f4"] = "toggle-preview",
+            ["shift-down"] = "preview-page-down",
+            ["shift-up"] = "preview-page-up",
+          },
+        },
         defaults = { formatter = "path.dirname_first" },
         hls = {
           title = "NvChHeadblue",
