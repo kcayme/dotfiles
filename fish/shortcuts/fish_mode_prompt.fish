57df6fba1dac29af1c53ba17946b4a13a461eea9
Karl <kcayme@gmail.com>
2025-08-12 21:58:42 +0800

    feat: add fish prompt functions

---
+++ fish/shortcuts/fish_mode_prompt.fish
@@ -0,0 +1,20 @@
+function fish_mode_prompt
+    # switch $fish_bind_mode
+    #     case default
+    #         set_color blue
+    #         echo -e "\n[N]"
+    #     case insert
+    #         set_color green
+    #         echo -e "\n[I]"
+    #     case replace_one
+    #         set_color red
+    #         echo "[R]"
+    #     case visual
+    #         set_color magenta
+    #         echo "[V]"
+    #     case *
+    #         set_color yellow
+    #         echo -n $fish_bind_mode 
+    # end
+    # set_color normal
+end
