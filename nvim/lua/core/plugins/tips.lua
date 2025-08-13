ba7f79edc99227ad0e2a306040ee41d29bedc1f7
Karl <kcayme@gmail.com>
2025-08-13 22:44:35 +0800

    feat: add neovim-tips

---
+++ nvim/lua/core/plugins/tips.lua
@@ -0,0 +1,13 @@
+return {
+  "saxon1964/neovim-tips",
+  dependencies = { "ibhagwan/fzf-lua" },
+  opts = {
+    user_file = vim.fn.stdpath("config") .. "/neovim_tips/user_tips.txt",
+  },
+  init = function()
+    local map = vim.keymap.set
+    map("n", "<leader>nto", ":NeovimTips<CR>", { desc = "Neovim tips", noremap = true, silent = true })
+    map("n", "<leader>nte", ":NeovimTipsEdit<CR>", { desc = "Edit your Neovim tips", noremap = true, silent = true })
+    map("n", "<leader>nta", ":NeovimTipsAdd<CR>", { desc = "Add your Neovim tip", noremap = true, silent = true })
+  end,
+}
