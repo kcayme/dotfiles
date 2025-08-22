024a192a8a7f8e76883e36585deec809fcaf6bf3
Karl <kcayme@gmail.com>
2025-08-22 08:34:29 +0800

    refactor: move user commands to commands.lua

---
+++ nvim/lua/core/config/commands.lua
@@ -0,0 +1,53 @@
+
+vim.api.nvim_create_user_command("FormatToggle", function(args)
+  local notify = require("notify")
+  local function show_notification(message, level)
+    notify(message, level, { title = "conform.nvim" })
+  end
+  local is_global = not args.bang
+  if is_global then
+    vim.g.disable_autoformat = not vim.g.disable_autoformat
+    if vim.g.disable_autoformat then
+      show_notification("Autoformat-on-save disabled globally", "info")
+    else
+      show_notification("Autoformat-on-save enabled globally", "info")
+    end
+  else
+    vim.b.disable_autoformat = not vim.b.disable_autoformat
+    if vim.b.disable_autoformat then
+      show_notification("Autoformat-on-save disabled for this buffer", "info")
+    else
+      show_notification("Autoformat-on-save enabled for this buffer", "info")
+    end
+  end
+end, {
+  desc = "Toggle autoformat-on-save",
+  bang = true,
+})
+
+-- Show linters for the current buffer's file type
+vim.api.nvim_create_user_command("LintInfo", function()
+  local filetype = vim.bo.filetype
+  local linters = require("lint").linters_by_ft[filetype]
+
+  if linters then
+    print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+  else
+    print("No linters configured for filetype: " .. filetype)
+  end
+end, {})
+
+-- https://github.com/folke/snacks.nvim/blob/main/docs/rename.md
+local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
+vim.api.nvim_create_autocmd("User", {
+  pattern = "NvimTreeSetup",
+  callback = function()
+    local events = require("nvim-tree.api").events
+    events.subscribe(events.Event.NodeRenamed, function(data)
+      if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
+        data = data
+        Snacks.rename.on_rename_file(data.old_name, data.new_name)
+      end
+    end)
+  end,
+})
