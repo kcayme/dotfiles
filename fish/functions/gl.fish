8e7326d9070ac76d21b491bc85e42e1af04931d6
Karl <kcayme@gmail.com>
2025-09-01 12:10:28 +0800

    feat(fish): update gl.fish config

--- fish/functions/gl.fish
+++ fish/functions/gl.fish
@@ -1,4 +1,17 @@
-function gl --wraps=git\ log\ --color\ --graph\ --pretty=format:\'\%Cred\%h\%Creset\ -\%C\(yellow\)\%d\%Creset\ \%s\ \%Cgreen\(\%cr\)\ \%C\(bold\ blue\)\<\%an\>\%Creset\'\ --abbrev-commit\ --branches --description alias\ gl=git\ log\ --color\ --graph\ --pretty=format:\'\%Cred\%h\%Creset\ -\%C\(yellow\)\%d\%Creset\ \%s\ \%Cgreen\(\%cr\)\ \%C\(bold\ blue\)\<\%an\>\%Creset\'\ --abbrev-commit\ --branches
-  git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches $argv
-        
+function gl --description "Pretty git log with graph and colors"
+    set format '%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'
+
+    if test (count $argv) -gt 0
+        # If you pass args (like a branch or commit range), just use those
+        git log --color --graph --pretty=format:$format --abbrev-commit $argv
+    else
+        # No args: default to showing all branches
+        git log --color --graph --pretty=format:$format --abbrev-commit --branches
+    end
 end
+
+
+# function gl --wraps=git\ log\ --color\ --graph\ --pretty=format:\'\%Cred\%h\%Creset\ -\%C\(yellow\)\%d\%Creset\ \%s\ \%Cgreen\(\%cr\)\ \%C\(bold\ blue\)\<\%an\>\%Creset\'\ --abbrev-commit\ --branches --description alias\ gl=git\ log\ --color\ --graph\ --pretty=format:\'\%Cred\%h\%Creset\ -\%C\(yellow\)\%d\%Creset\ \%s\ \%Cgreen\(\%cr\)\ \%C\(bold\ blue\)\<\%an\>\%Creset\'\ --abbrev-commit\ --branches
+#   git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches $argv
+#         
+# end
