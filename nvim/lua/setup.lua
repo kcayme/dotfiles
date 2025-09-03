503a6b5cb18cd38bf9ab25851ed73e0980b4fbb5
Karl <kcayme@gmail.com>
2025-09-03 09:59:41 +0800

    chore(nvim): update lazy config

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -44,23 +44,23 @@ require("lazy").setup({
     },
   },
   checker = { enabled = true },
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = "⌘",
-      config = "🛠",
-      event = "📅",
-      ft = "📂",
-      init = "⚙",
-      keys = "🗝",
-      plugin = "🔌",
-      runtime = "💻",
-      require = "🌙",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-      lazy = "💤 ",
-    },
-  },
+  -- ui = {
+  -- icons = vim.g.have_nerd_font and {} or {
+  --   cmd = "⌘",
+  --   config = "🛠",
+  --   event = "📅",
+  --   ft = "📂",
+  --   init = "⚙",
+  --   keys = "🗝",
+  --   plugin = "🔌",
+  --   runtime = "💻",
+  --   require = "🌙",
+  --   source = "📄",
+  --   start = "🚀",
+  --   task = "📌",
+  --   lazy = "💤 ",
+  -- },
+  -- },
 })
 
 -- Load appropriate plugins, settings, and keymaps
