d79db512f0fe0629b5910925b14fee229c4ba265
Karl Cayme <kcayme@agsx.net>
2025-09-04 18:33:13 +0800

    feat(nvim): add neogen

--- nvim/lua/core/plugins/comment.lua
+++ nvim/lua/core/plugins/comment.lua
@@ -4,11 +4,36 @@ return {
     "numToStr/Comment.nvim",
     opts = {},
     config = function()
-      require("Comment").setup()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
 
+      local api = require("Comment.api")
       local map = vim.api.nvim_set_keymap
       map("n", "<C-_>", "gcc", { noremap = false, desc = "Toggle linewise comment" })
       map("v", "<C-_>", "gcc", { noremap = false, desc = "Toggle blockwise comment" })
+      vim.keymap.set("x", "<leader><C-_>", function()
+        vim.api.nvim_feedkeys(esc, "nx", false)
+        api.toggle.blockwise(vim.fn.visualmode())
+      end, { noremap = false, desc = "Toggle blockwise comment" })
     end,
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+
+      local get_option = vim.filetype.get_option
+      vim.filetype.get_option = function(filetype, option)
+        return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+          or get_option(filetype, option)
+      end
+    end,
+  },
+  {
+    "danymat/neogen",
+    config = true,
   },
 }
