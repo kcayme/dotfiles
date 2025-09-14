ac3e65ee18818c7a78b07616872b146380c768ca
Karl <kcayme@gmail.com>
2025-09-14 12:11:46 +0800

    feat(nvim): add render-markdown.nvim

--- nvim/lua/core/plugins/previewers.lua
+++ nvim/lua/core/plugins/previewers.lua
@@ -24,4 +24,16 @@ return {
     end,
     ft = { "markdown" },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    config = function()
+      require("render-markdown").setup({
+        completions = { lsp = { enabled = true } },
+      })
+    end,
+  },
 }
