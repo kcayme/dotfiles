68c28c774df346a831498d94cfeb46dcb41bc432
Karl Cayme <kcayme@agsx.net>
2025-09-16 12:44:22 +0800

    feat(nvim): update lua_ls config

--- nvim/lua/core/plugins/lsp.lua
+++ nvim/lua/core/plugins/lsp.lua
@@ -41,10 +41,10 @@ return {
                 checkThirdParty = false,
                 -- Tells lua_ls where to find all the Lua files that you have loaded
                 -- for your neovim configuration.
-                library = {
-                  "${3rd}/luv/library",
-                  unpack(vim.api.nvim_get_runtime_file("", true)),
-                },
+                -- library = {
+                --   "${3rd}/luv/library",
+                --   unpack(vim.api.nvim_get_runtime_file("", true)),
+                -- },
               },
               completion = {
                 callSnippet = "Replace",
