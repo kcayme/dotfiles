61d37ad2ad45537b469e40c474aa32ee0f098d1a
Karl Cayme <kcayme@agsx.net>
2025-09-30 17:12:10 +0800

    feat(nvim): update lsp config

--- nvim/lua/core/config/lsp.lua
+++ nvim/lua/core/config/lsp.lua
@@ -1,12 +1,6 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-capabilities = vim.tbl_deep_extend(
-  "force",
-  capabilities,
-  {}
-  -- require("cmp_nvim_lsp").default_capabilities(capabilities),
-  -- require("blink.cmp").get_lsp_capabilities(capabilities)
-)
+
+capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities(capabilities))
 
 vim.lsp.config("*", {
   capabilities = capabilities,
