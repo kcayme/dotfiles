ac3808b5c933d25150a2fda06d3e54befe2f34ca
Karl <kcayme@gmail.com>
2025-10-01 11:18:38 +0800

    feat(nvim): update lsp capabilities setup

--- nvim/lua/core/config/lsp.lua
+++ nvim/lua/core/config/lsp.lua
@@ -2,6 +2,16 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities(capabilities))
 
+-- merge custom capabilities
+capabilities = vim.tbl_deep_extend("force", capabilities, {
+  textDocument = {
+    semanticTokens = {
+      multilineTokenSupport = true,
+    },
+  },
+})
+
 vim.lsp.config("*", {
   capabilities = capabilities,
+  root_markers = { ".git" },
 })
