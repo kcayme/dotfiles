23deb8ac1ba5ed6e1c31fce045e4b47a73b621fa
Karl <kcayme@gmail.com>
2025-10-08 12:44:46 +0800

    feat(nvim): update formatters for python

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -11,16 +11,21 @@ return {
       default_format_opts = {
         lsp_format = "fallback",
       },
-      format_on_save = function(bufnr)
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_fallback = true }
-      end,
+      -- format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      -- if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      --   return
+      -- end
+      -- return { timeout_ms = 500, lsp_fallback = true }
+      --
+      -- end,
+      format_on_save = {
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "ruff", "autopep8", stop_after_first = false },
+        python = { "ruff", "autopep8", "isort", "docformatter", stop_after_first = false },
         go = { "goimports", "gofmt", stop_after_first = true },
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
