4fdb761d42e2f634fd3d7035337b5cde8fcd75d4
Karl Cayme <karlcayme@Amihans-MacBook-Pro.local>
2025-10-22 13:15:35 +0800

    feat(nvim): add custom highlights for bufferline custom areas

--- nvim/lua/core/config/highlights.lua
+++ nvim/lua/core/config/highlights.lua
@@ -4,32 +4,39 @@ for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
   dofile(vim.g.base46_cache .. v)
 end
 
--- support undercurl on diagnostic highlights
--- local colors = require("base46").get_theme_tb("base_30")
---
--- local hl_color_map = {
---   {
---     group_name = "DiagnosticUnderlineError",
---     opts = { sp = colors.red },
---   },
---   {
---     group_name = "DiagnosticUnderlineWarn",
---     opts = { sp = colors.eared },
---   },
---   {
---     group_name = "DiagnosticUnderlineInfo",
---     opts = { sp = colors.red },
---   },
---   {
---     group_name = "DiagnosticUnderlineHint",
---     opts = { sp = colors.red },
---   },
---   {
---     group_name = "BlinkCmpGhostText",
---     opts = { fg = "#4e5665" },
---   },
--- }
+local colors = require("utils.colors").get_base30_palette()
 
--- for _, value in ipairs(hl_color_map) do
---   vim.api.nvim_set_hl(0, value.group_name, value.opts)
--- end
+local custom_hls = {
+  {
+    name = "RoundedEdge",
+    opts = { fg = colors and colors.blue or "#000000", bg = "NONE" },
+  },
+  {
+    name = "RoundedInner",
+    opts = { fg = colors and colors.black or "#ffffff", bg = colors and colors.blue or "#000000" },
+  },
+  --   {
+  --     group_name = "DiagnosticUnderlineError",
+  --     opts = { sp = colors.red },
+  --   },
+  --   {
+  --     group_name = "DiagnosticUnderlineWarn",
+  --     opts = { sp = colors.eared },
+  --   },
+  --   {
+  --     group_name = "DiagnosticUnderlineInfo",
+  --     opts = { sp = colors.red },
+  --   },
+  --   {
+  --     group_name = "DiagnosticUnderlineHint",
+  --     opts = { sp = colors.red },
+  --   },
+  --   {
+  --     group_name = "BlinkCmpGhostText",
+  --     opts = { fg = "#4e5665" },
+  --   },
+}
+
+for _, value in ipairs(custom_hls) do
+  vim.api.nvim_set_hl(0, value.name, value.opts)
+end
