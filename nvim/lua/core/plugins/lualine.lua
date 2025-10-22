fbb7d1bd32d16fc94d7681a2efaca532bad0d671
Karl Cayme <karlcayme@Amihans-MacBook-Pro.local>
2025-10-22 13:21:06 +0800

    feat(nvim): update lualine separators

--- nvim/lua/core/plugins/lualine.lua
+++ nvim/lua/core/plugins/lualine.lua
@@ -56,7 +56,7 @@ return {
         options = {
           icons_enabled = true,
           theme = custom_theme,
-          -- component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
           component_separators = { left = "", right = "" },
           -- section_separators = { left = "", right = "" },
           disabled_filetypes = {
