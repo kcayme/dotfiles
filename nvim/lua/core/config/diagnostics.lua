7ca0925969f32f27f81b6469576c208ccb15d371
Karl <kcayme@gmail.com>
2025-10-27 23:00:27 +0800

    refactor: move custom diagnostic jump command to diagnostics.lua

--- nvim/lua/core/config/diagnostics.lua
+++ nvim/lua/core/config/diagnostics.lua
@@ -1,3 +1,4 @@
+local M = {}
 local custom_signs = require("core.themes.signs")
 vim.diagnostic.config({
   signs = {
@@ -9,3 +10,29 @@ vim.diagnostic.config({
     },
   },
 })
+
+-- custom command to toggle diagnostic virtual line when jumping to next diagnostic
+M.jumpWithVirtLineDiags = function(jumpCount)
+  pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
+
+  vim.diagnostic.jump({ count = jumpCount })
+
+  local initialVirtTextConf = vim.diagnostic.config().virtual_text
+  vim.diagnostic.config({
+    virtual_text = false,
+    virtual_lines = { current_line = true },
+  })
+
+  vim.defer_fn(function() -- deferred to not trigger by jump itself
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      desc = "User(once): Reset diagnostics virtual lines",
+      once = true,
+      group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
+      callback = function()
+        vim.diagnostic.config({ virtual_lines = false, virtual_text = initialVirtTextConf })
+      end,
+    })
+  end, 1)
+end
+
+return M
--- nvim/lua/core/config/keymaps.lua
+++ nvim/lua/core/config/keymaps.lua
@@ -8,35 +8,13 @@ map("n", "<Esc>", function()
 end, { desc = "Clear notifs and highlights" })
 
 -- INFO: DIAGNOSTICS =====================================================================
----@param jumpCount number
-local function jumpWithVirtLineDiags(jumpCount)
-  pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
-
-  vim.diagnostic.jump({ count = jumpCount })
-
-  local initialVirtTextConf = vim.diagnostic.config().virtual_text
-  vim.diagnostic.config({
-    virtual_text = false,
-    virtual_lines = { current_line = true },
-  })
-
-  vim.defer_fn(function() -- deferred to not trigger by jump itself
-    vim.api.nvim_create_autocmd("CursorMoved", {
-      desc = "User(once): Reset diagnostics virtual lines",
-      once = true,
-      group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
-      callback = function()
-        vim.diagnostic.config({ virtual_lines = false, virtual_text = initialVirtTextConf })
-      end,
-    })
-  end, 1)
-end
-
 map("n", "<leader>]", function()
-  jumpWithVirtLineDiags(1)
+  vim.diagnostic.jump({ count = 1 })
+  -- jumpWithVirtLineDiags(1)
 end, { desc = "󰒕 Next diagnostic" })
 map("n", "<leader>[", function()
-  jumpWithVirtLineDiags(-1)
+  vim.diagnostic.jump({ count = -1 })
+  --   jumpWithVirtLineDiags(-1)
 end, { desc = "󰒕 Prev diagnostic" })
 
 -- INFO: NAVIGATIONS ====================================================================
