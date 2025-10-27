ea35282ba687f247e33dbc554d89b37a3e5da5b1
Karl <kcayme@gmail.com>
2025-10-27 23:41:27 +0800

    chore: add util for fetching base16 palette

--- nvim/lua/utils/colors.lua
+++ nvim/lua/utils/colors.lua
@@ -17,4 +17,19 @@ M.get_base30_palette = function()
   return nil
 end
 
+M.get_base16_palette = function()
+  if M.colors then
+    return M.colors
+  end
+
+  local ok, base46 = pcall(require, "base46")
+  if ok then
+    M.colors = base46.get_theme_tb("base_16")
+
+    return M.colors
+  end
+
+  return nil
+end
+
 return M
