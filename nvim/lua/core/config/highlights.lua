b378d9404de796edac3eab82532fa263dfd63061
Karl Cayme <karlcayme@Amihans-MacBook-Pro.local>
2025-11-06 20:41:55 +0800

    refactor: move base46 cache setup before lazy setup

--- nvim/lua/core/config/highlights.lua
+++ nvim/lua/core/config/highlights.lua
@@ -1,5 +1,4 @@
 -- apply nvchad base46
-vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
 for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
   dofile(vim.g.base46_cache .. v)
 end
