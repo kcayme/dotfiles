6d69a0b1429e8b01d8fcd48532d47665eca50e08
Karl Cayme <karlcayme@Amihans-MacBook-Pro.local>
2025-11-13 09:51:18 +0800

    chore: update conform config

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -11,18 +11,13 @@ return {
       default_format_opts = {
         lsp_format = "fallback",
       },
-      -- format_on_save = function(bufnr)
-      -- Disable with a global or buffer-local variable
-      -- if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-      --   return
-      -- end
-      -- return { timeout_ms = 500, lsp_fallback = true }
-      --
-      -- end,
-      format_on_save = {
-        lsp_format = "fallback",
-        timeout_ms = 500,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
       formatters_by_ft = {
         toml = { "taplo" },
         lua = { "stylua" },
