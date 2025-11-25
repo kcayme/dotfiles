5434c68dc64bd0626c1414e84f31a1ea36117133
Karl <kcayme@gmail.com>
2025-11-25 09:57:21 +0800

    chore: update flash.nvim keymap and hl

--- nvim/lua/core/plugins/flash.lua
+++ nvim/lua/core/plugins/flash.lua
@@ -13,7 +13,7 @@ return {
     keys = {
       -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       {
-        "s",
+        "S",
         mode = { "n", "o", "x" },
         function()
           require("flash").treesitter({
