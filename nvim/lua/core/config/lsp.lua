b4a4c168adebf2b67d5242334e67fec7d54aa67b
Karl <kcayme@gmail.com>
2025-12-15 00:29:54 +0800

    revert: cleanup vtsls config

--- nvim/lua/core/config/lsp.lua
+++ nvim/lua/core/config/lsp.lua
@@ -11,6 +11,35 @@ capabilities = vim.tbl_deep_extend("force", capabilities, {
   },
 })
 
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+  .. "/vue-language-server"
+  .. "/node_modules/@vue/language-server"
+
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+local vue_plugin = {
+  name = "@vue/typescript-plugin",
+  location = vue_language_server_path,
+  languages = { "vue" },
+  configNamespace = "typescript",
+}
+local vtsls_config = {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
+        },
+      },
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+-- local vue_ls_config = {}
+vim.lsp.config("vtsls", vtsls_config)
+-- vim.lsp.config("vue_ls", vue_ls_config)
+-- vim.lsp.enable({ "vtsls", "vue_ls" })
+
 vim.lsp.config("*", {
   capabilities = capabilities,
   root_markers = { ".git" },
