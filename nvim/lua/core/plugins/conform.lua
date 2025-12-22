9207d051554647ddde1c17c9c85c933892f16b12
Karl <kcayme@gmail.com>
2025-12-22 23:12:53 +0800

    feat: set formatter for json files

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -39,7 +39,7 @@ return {
         typescript = { "prettierd", "prettier", stop_after_first = true },
         html = { "prettierd", "prettier", stop_after_first = true },
         css = { "prettierd", stop_after_first = true },
-        -- json = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
         markdown = { "prettierd", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
