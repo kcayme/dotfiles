f115c4a755ec0ab7f4aa0e58b85a92e30b0a0055
Karl <kcayme@gmail.com>
2025-12-27 01:01:18 +0800

    feat: integrate treesitter queries to mini.ai

--- nvim/lua/core/plugins/mini.lua
+++ nvim/lua/core/plugins/mini.lua
@@ -2,8 +2,24 @@ return {
   {
     "echasnovski/mini.ai",
     version = "*",
+    dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
     config = function()
-      require("mini.ai").setup()
+      local gen_spec = require("mini.ai").gen_spec
+      require("mini.ai").setup({
+        custom_textobjects = {
+          -- Tweak argument to be recognized only inside `()` between `;`
+          -- a = gen_spec.argument({ brackets = { "%b()" }, separator = ";" }),
+
+          -- Tweak function call to not detect dot in function name
+          -- f = gen_spec.function_call({ name_pattern = "[%w_]" }),
+
+          -- Function definition (needs treesitter queries with these captures)
+          F = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+
+          -- Make `|` select both edges in non-balanced way
+          -- ["|"] = gen_spec.pair("|", "|", { type = "non-balanced" }),
+        },
+      })
     end,
   },
 }
