b503a0d53ef5cd1f47727b485773511d08ece2f8
Karl Cayme <kcayme@gmail.com>
2026-01-14 08:56:26 +0800

    feat: set mdformat as formatter

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -40,7 +40,7 @@ return {
         html = { "prettierd", "prettier", stop_after_first = true },
         css = { "prettierd", stop_after_first = true },
         json = { "prettierd", "prettier", stop_after_first = true },
-        markdown = { "prettierd", stop_after_first = true },
+        markdown = { "mdformat", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         vue = { "prettierd", "prettier", stop_after_first = true },
