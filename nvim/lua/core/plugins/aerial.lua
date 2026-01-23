907aadd883b7b741e3136f964405c62e6ef3bae3
Karl Cayme <kcayme@gmail.com>
2026-01-23 08:33:24 +0800

    feat: change AerialToggle keymap

--- nvim/lua/core/plugins/aerial.lua
+++ nvim/lua/core/plugins/aerial.lua
@@ -30,7 +30,7 @@ return {
         -- Jump forwards/backwards with '{' and '}'
         -- vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
         -- vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
+        vim.keymap.set("n", "<C-a>", "<cmd>AerialToggle<CR>")
       end,
     })
   end,
