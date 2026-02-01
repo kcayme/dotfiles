f36f97f55a249b1da734ee004986be7f5f1c92a0
Karl Cayme <kcayme@gmail.com>
2026-02-01 18:20:32 +0800

    feat: update gopls config and use vim-go for consistent syntax highlighting over go.nvim

--- nvim/lsp/gopls.lua
+++ nvim/lsp/gopls.lua
@@ -1,1 +1,33 @@
-return {}
+return {
+  settings = {
+    gopls = {
+      analyses = {
+        -- check analyzers for default values
+        -- leeave most of them to default
+        -- shadow = true,
+        unusedvariable = true,
+        useany = true,
+      },
+      codelenses = {
+        generate = true, -- show the `go generate` lens.
+        gc_details = true, -- Show a code lens toggling the display of gc's choices.
+        test = true,
+        tidy = true,
+        vendor = true,
+        regenerate_cgo = true,
+        upgrade_dependency = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+}
