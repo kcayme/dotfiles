4569c4f1258859322e6e9c2498c5240c940050fa
Karl Cayme <kcayme@gmail.com>
2026-02-12 11:27:54 +0800

    feat: use sidekick's focus command when switching from file buffer to sidekick.nvim buffer

--- nvim/lua/core/plugins/tmux-nav.lua
+++ nvim/lua/core/plugins/tmux-nav.lua
@@ -11,6 +11,14 @@ return {
     { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
     { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
     { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    {
+      "<c-l>",
+      function()
+        local ok = pcall(vim.cmd, "Sidekick cli focus")
+        if not ok then
+          vim.cmd("TmuxNavigateRight")
+        end
+      end,
+    },
   },
 }
