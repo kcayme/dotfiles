3cf7461006bfacd8fae8546627d640f8b09e8e61
Karl Cayme <kcayme@gmail.com>
2026-02-12 15:30:59 +0800

    refactor: per file configs for lsps

--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -1,7 +1,25 @@
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+  .. "/vue-language-server"
+  .. "/node_modules/@vue/language-server"
+
 return {
   cmd = { "vtsls", "--stdio" },
   init_options = {
     hostInfo = "neovim",
+  },
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vue_language_server_path,
+            languages = { "vue" },
+            configNamespace = "typescript",
+          },
+        },
+      },
+    },
   },
   filetypes = {
     "vue",
