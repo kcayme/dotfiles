9d7dafb9415ec751ef9885e0bc4de720dedbeca2
Karl Cayme <kcayme@gmail.com>
2026-02-16 10:11:59 +0800

    feat: replace hop.nvim with flash for 2-char jumps

--- nvim/lua/core/config/keymaps.lua
+++ nvim/lua/core/config/keymaps.lua
@@ -141,6 +141,61 @@ map({ "n", "x", "o" }, "<leader>s", function()
   })
 end, { desc = "Treesitter incremental selection" })
 
+map({ "n", "v" }, "<Leader>w", function()
+  local Flash = require("flash")
+  Flash.jump({
+    search = { mode = "search" },
+    label = {
+      after = false,
+      before = { 0, 0 },
+      uppercase = false,
+      format = function(opts)
+        return {
+          { opts.match.label1, "FlashLabel" },
+          { opts.match.label2, "Comment" },
+        }
+      end,
+    },
+    pattern = [[\<]],
+    action = function(match, state)
+      state:hide()
+      Flash.jump({
+        search = { max_length = 0 },
+        highlight = { matches = false },
+        label = {
+          after = false,
+          before = { 0, 0 },
+          format = function(opts)
+            return {
+              { opts.match.label1, "Comment" },
+              { opts.match.label2, "FlashMatch" },
+            }
+          end,
+        },
+        matcher = function(win)
+          -- limit matches to the current label
+          return vim.tbl_filter(function(m)
+            return m.label == match.label and m.win == win
+          end, state.results)
+        end,
+        labeler = function(matches)
+          for _, m in ipairs(matches) do
+            m.label = m.label2 -- use the second label
+          end
+        end,
+      })
+    end,
+    labeler = function(matches, state)
+      local labels = state:labels()
+      for m, match in ipairs(matches) do
+        match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+        match.label2 = labels[(m - 1) % #labels + 1]
+        match.label = match.label1
+      end
+    end,
+  })
+end, { noremap = true, desc = "Hop next words" })
+
 -- INFO: PICKERS =======================================
 local Picker = require("core.config.interfaces")
 
