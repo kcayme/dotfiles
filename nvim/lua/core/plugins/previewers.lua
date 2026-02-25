de746fd0b5dded7e5c20946759279759a8cd6ad9
Karl Cayme <kcayme@gmail.com>
2026-02-25 10:40:09 +0800

    feat: replace live-server

--- nvim/lua/core/plugins/previewers.lua
+++ nvim/lua/core/plugins/previewers.lua
@@ -21,24 +21,35 @@ return {
       "folke/snacks.nvim",
     },
   },
-  -- {
-  --   "MeanderingProgrammer/render-markdown.nvim",
-  --   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-  --   ---@module 'render-markdown'
-  --   ---@type render.md.UserConfig
-  --   opts = {},
-  --   config = function()
-  --     require("render-markdown").setup({
-  --       completions = { lsp = { enabled = true } },
-  --     })
-  --   end,
-  -- },
+  {
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    config = function()
+      require("markdown_preview").setup({
+        -- all optional; sane defaults shown
+        port = 8421,
+        open_browser = true,
+        debounce_ms = 300,
+      })
+    end,
+  },
   {
     "OXY2DEV/markview.nvim",
     lazy = false,
-    keys = {
-      { "<leader>md", "<cmd>Markview toggle<cr>", mode = "n", desc = "Toggle MarkView" },
-    },
     dependencies = { "saghen/blink.cmp" },
+    config = function()
+      local presets = require("markview.presets").headings
+
+      require("markview").setup({
+        preview = {
+          enable = false,
+          icon_provider = "devicons", -- "mini" or "devicons"
+          filetypes = { "markdown" },
+        },
+        markdown = { headings = presets.numbered },
+      })
+      local map = require("utils.keymap").map
+      map("n", "<leader>md", "<CMD>Markview splitToggle<CR>", { desc = "Split toggles `markview` preview" })
+    end,
   },
 }
