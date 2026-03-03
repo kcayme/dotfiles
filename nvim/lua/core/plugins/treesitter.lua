c7eb28a7879585c00728f286e2326b20ba5f440a
Karl Cayme <kcayme@gmail.com>
2026-03-03 14:42:51 +0800

    feat: update plugins

--- nvim/lua/core/plugins/treesitter.lua
+++ nvim/lua/core/plugins/treesitter.lua
@@ -36,6 +36,12 @@ return {
       }
 
       require("nvim-treesitter").install(ensure_installed)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
     end,
   },
   {
