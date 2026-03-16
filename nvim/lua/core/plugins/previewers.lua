2ed44910415003b4c67898a6f973eb69eb09fc72
Karl Cayme <kcayme@gmail.com>
2026-03-16 21:50:00 +0800

    feat: remove unused plugins

--- nvim/lua/core/plugins/previewers.lua
+++ nvim/lua/core/plugins/previewers.lua
@@ -21,34 +21,4 @@ return {
       "folke/snacks.nvim",
     },
   },
-  {
-    "selimacerbas/markdown-preview.nvim",
-    dependencies = { "selimacerbas/live-server.nvim" },
-    config = function()
-      require("markdown_preview").setup({
-        -- all optional; sane defaults shown
-        port = 8421,
-        open_browser = true,
-        debounce_ms = 300,
-      })
-    end,
-  },
-  {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    config = function()
-      local presets = require("markview.presets").headings
-
-      require("markview").setup({
-        preview = {
-          enable = false,
-          icon_provider = "devicons", -- "mini" or "devicons"
-          filetypes = { "markdown" },
-        },
-        markdown = { headings = presets.numbered },
-      })
-      local map = require("utils.keymap").map
-      map("n", "<leader>md", "<CMD>Markview splitToggle<CR>", { desc = "Split toggles `markview` preview" })
-    end,
-  },
 }
