776f6692ab4b9bafb1a437b050604e61451871cc
Karl Cayme <kcayme@gmail.com>
2026-05-29 01:03:31 +0800

    refactor: unified Picker

--- nvim/lua/core/config/pickers/snacks.lua
+++ nvim/lua/core/config/pickers/snacks.lua
@@ -1,32 +1,7 @@
 local picker = require("snacks").picker
 
-local snacks = {}
+local M = {}
 
-local picker_interface_methods = require("core.config.pickers.interfaces").picker_interface_methods
+M.picker = picker
 
-for _, method in ipairs(picker_interface_methods) do
-  snacks[method] = function(opts)
-    picker[method](opts)
-  end
-end
-
--- Custom logic
-snacks.buffers = function(opts)
-  opts = opts or {}
-  opts.filter = vim.tbl_extend("force", opts.filter or {}, { cwd = true })
-  picker.buffers(opts)
-end
-
-snacks.jumps = function(opts)
-  opts = opts or {}
-  opts.filter = vim.tbl_extend("force", opts.filter or {}, { cwd = true })
-  picker.jumps(opts)
-end
-
-snacks.global = function(opts)
-  -- opts = opts or {}
-  -- opts.filter = vim.tbl_extend("force", opts.filter or {}, { cwd = true })
-  picker.lsp_symbols(opts)
-end
-
-return snacks
+return M
